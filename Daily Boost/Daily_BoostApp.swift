ebdac5e4e0ba7341a4ca15a07342f2b1f8ef95f1
long nguyen <elongmusk39@gmail.com>
2024-05-22 15:31:41 -0700

    Initial Commit

---
+++ Daily Boost/Daily_BoostApp.swift
@@ -0,0 +1,17 @@
+//
+//  Daily_BoostApp.swift
+//  Daily Boost
+//
+//  Created by Long Nguyen on 5/22/24.
+//
+
+import SwiftUI
+
+@main
+struct Daily_BoostApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
